bb831debe7e4a1c1933c1ac639ffd75ff334f128
Piyush Sharan <piyushsharan.ece18@iitbhu.ac.in>
2023-08-21 13:31:46 -0500

    Initial setup

---
+++ src/main/java/com/ems/ems/EmsApplication.java
@@ -0,0 +1,15 @@
+package com.ems.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class EmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
